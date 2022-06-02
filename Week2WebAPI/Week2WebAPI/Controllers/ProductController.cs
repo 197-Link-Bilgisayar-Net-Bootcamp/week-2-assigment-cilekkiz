d175ba695bb81670a078a3f31f70a3d4d84ca322
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2022-06-03 02:21:41 +0300

    productcontroller ve CRUD endpointleri eklendi.

---
+++ Week2WebAPI/Week2WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Week2WebAPI.Core.DTO;
+using Week2WebAPI.Services;
+
+namespace Week2WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/{action}")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ProductDTO>>> GetAllProducts()
+        {
+            return await _productService.GetAllProductsAsync();
+        }
+        [HttpGet("{Id:Guid}")]
+        public async Task<ActionResult<ProductDTO>> GetProduct(Guid Id)
+        {
+            return await _productService.GetProductAsync(Id);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ProductDTO>> CreateProduct(ProductDTO newEntity)
+        {
+            return await _productService.CreateProductAsync(newEntity);
+        }
+        [HttpPut]
+        public async Task<ActionResult<ProductDTO>> UpdateProduct(ProductDTO entity)
+        {
+            return await _productService.UpdateProductAsync(entity);
+        }
+        [HttpDelete("{Id:Guid}")]
+        public async Task<ActionResult<ProductDTO>> DeleteProduct(Guid Id)
+        {
+            return await _productService.DeleteProductAsync(Id);
+        }
+    }
+}
