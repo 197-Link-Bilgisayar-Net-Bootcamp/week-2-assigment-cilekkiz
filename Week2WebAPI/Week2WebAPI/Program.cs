024b30246b52d79c519588f85f8f887f3ec195f7
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2022-06-03 02:25:20 +0300

    program.cs dosyasi duzenlendi.

---
+++ Week2WebAPI/Week2WebAPI/Program.cs
@@ -0,0 +1,86 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Week2WebAPI.Core.Mapping;
+using Week2WebAPI.Core.Models;
+using Week2WebAPI.Core.Shared.Interfaces;
+using Week2WebAPI.Infrastructure.Database;
+using Week2WebAPI.Infrastructure.Repository;
+using Week2WebAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddDbContext<WebAPIContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("WebAPIConnection")));
+builder.Services.AddScoped<IAsyncRepository<Product>, AsyncRepository<Product>>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        name: "_myAllowOrigins",
+        builder => builder.WithOrigins("http://localhost:5210")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .SetIsOriginAllowed((host) => true));
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Week2WebAPI", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
+                    }
+                });
+});
+
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors("_myAllowOrigins");
+
+app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Week2WebAPI v1"));
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+//app.MapControllers();
+
+//app.Run();
