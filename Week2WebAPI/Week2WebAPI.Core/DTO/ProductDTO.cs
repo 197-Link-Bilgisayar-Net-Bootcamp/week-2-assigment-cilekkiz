812ee02cd3879c02deb32680af064096f364de3c
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2022-06-03 02:25:46 +0300

    Product db modeli icin ProductDTO isimli DTO model olusturuldu.

---
+++ Week2WebAPI/Week2WebAPI.Core/DTO/ProductDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Week2WebAPI.Core.DTO
+{
+    public class ProductDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
