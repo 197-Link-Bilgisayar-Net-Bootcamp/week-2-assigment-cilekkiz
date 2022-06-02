c8241c157a86d86e9a4a105780701682472f4501
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2022-06-03 02:27:46 +0300

    DbContext class indan türeyen bir WebAPIContext class i olusturuldu.

---
+++ Week2WebAPI/Week2WebAPI.Infrastructure/Database/WebAPIContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Week2WebAPI.Core.Models;
+
+namespace Week2WebAPI.Infrastructure.Database
+{
+    public class WebAPIContext : DbContext
+    {
+        public WebAPIContext(DbContextOptions<WebAPIContext> options) : base(options)
+        { 
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
