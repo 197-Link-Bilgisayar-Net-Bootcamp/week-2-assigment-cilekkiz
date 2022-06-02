7df7a3882a24218d59f00183ef359b5b236a4e24
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2022-06-03 02:28:32 +0300

    Automapper kuruldu mapleme islemleri icin mapping profile class i eklendi.

---
+++ Week2WebAPI/Week2WebAPI.Core/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Week2WebAPI.Core.DTO;
+using Week2WebAPI.Core.Models;
+
+namespace Week2WebAPI.Core.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
