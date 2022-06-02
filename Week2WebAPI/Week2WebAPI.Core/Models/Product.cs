c55fa93b18b42ea729f45921467217d36d8a13f2
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2022-06-03 02:28:03 +0300

    product db model olusturuldu.

---
+++ Week2WebAPI/Week2WebAPI.Core/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week2WebAPI.Core.Shared;
+
+namespace Week2WebAPI.Core.Models
+{
+    public class Product : BaseEntity<Guid>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
